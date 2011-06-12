0167190ec9c06d378ce28865e80dd4ba193aed66
vkris <vivekrishna@gmail.com>
2011-06-12 15:05:23 -0400

    Look up file

---
+++ gsbloomboost/lookup.py
@@ -0,0 +1,17 @@
+class LookUp:
+    """
+    A class to manipulate the charlotte's 400 list.
+    """
+    def __init__(self):
+        """ 
+        Generate the hash file here.
+        Should ideally use two files, the xml file containing negative Ids
+        and the charlott's list
+        The resultant hash if of hte form -id->[list of elements]
+        """
+        pass
+    def get_elements(self):
+        """
+        Get the list of elements for a particular id
+        """
+        
